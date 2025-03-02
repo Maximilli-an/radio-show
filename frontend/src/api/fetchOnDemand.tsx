3d6cafbc6177f97127a9af055a99eb50f9c89239
Max A <thenasher@hotmail.com>
2025-03-03 09:29:19 +1100

    better error handling on api call

--- frontend/src/api/fetchOnDemand.tsx
+++ frontend/src/api/fetchOnDemand.tsx
@@ -2,12 +2,17 @@ import { useQuery } from '@tanstack/react-query';
 import { OnDemandPayload } from '../types/OnDemand';
 
 const fetchOnDemandContent = async (): Promise<OnDemandPayload> => {
-  const response = await fetch('api/2gb/feed/onDemand/249')
-  if (!response.ok) {
-    throw new Error('Bad network response, please try again later')
-  }
-  return response.json()
-};
+  try {
+    const response = await fetch('api/2gb/feed/onDemand/249')
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    return await response.json()
+  } catch (error) {
+    console.error('Error fetching on-demand content:', error)
+    throw error;
+}
+}
 
 //RQ hook for actually getting the content
 export const useOnDemandContent = () => {
